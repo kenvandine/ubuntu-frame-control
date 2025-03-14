c7f2fa87299f1eedd462c3f736d77102275a1cca
Drew VanDine <drew@vandine.org>
2025-03-14 13:52:26 -0400

    feat: network manager integration

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import psutil
 import pulsectl
 import subprocess
 import socket
+import os
+import time
+import math
 
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
-from gi.repository import Gtk, Adw, GLib, Gio
+gi.require_version('NM', '1.0')
+from gi.repository import Gtk, Adw, GLib, Gio, NM
+
+SCAN_THRESHOLD_MSEC = 10000
 
 class UbuntuFrameControlWindow(Adw.ApplicationWindow):
 
@@ -37,7 +43,16 @@ class UbuntuFrameControlWindow(Adw.ApplicationWindow):
         ip_row.add_suffix(self.ip_value)
 
         self.update_ip_address()
-
+        
+        # Wifi Heading
+        wifi = Adw.ActionRow()
+        wifi.set_title("<b>WiFi SSIDs</b>")
+        self.box.append(wifi)
+        self.wifi_selector = Gtk.ComboBoxText()
+        self.wifi_selector.append_text("Select WiFi SSID")
+        self.wifi_selector.set_size_request(150, -1)
+        wifi.add_suffix(self.wifi_selector)
+        
         # Audio Devices Heading
         audio_devices_heading = Adw.ActionRow()
         audio_devices_heading.set_title("<b>Audio Devices</b>")
@@ -97,6 +112,11 @@ class UbuntuFrameControlWindow(Adw.ApplicationWindow):
         self.input_combo.connect("changed", self.on_input_device_changed)
         self.output_volume.connect("value-changed", self.on_output_volume_changed)
         self.input_volume.connect("value-changed", self.on_input_volume_changed)
+        
+        self.nmc = NM.Client.new(None)
+        self.devs = self.nmc.get_devices()
+
+        self.update_wifi_devices()
 
     def update_battery_info(self):
         battery = psutil.sensors_battery()
@@ -152,6 +172,40 @@ class UbuntuFrameControlWindow(Adw.ApplicationWindow):
                 self.input_combo.set_active(i + 1)
                 self.input_volume.set_value(dev.volume.value_flat * 100)
 
+    def update_wifi_devices(self):
+        # Get WiFi devices
+        self.wifi_selector.remove_all()
+        self.wifi_selector.append_text("Select WiFi SSID")
+        is_first = True
+        
+        def text_exists_in_combobox(combobox, text):
+            model = combobox.get_model()
+            iter = model.get_iter_first()
+            while iter is not None:
+                if model[iter][0] == text:
+                    return True
+                iter = model.iter_next(iter)
+            return False
+        
+        for device in self.devs:
+            print(device.get_device_type())
+            if device.get_device_type() != NM.DeviceType.WIFI:
+                continue
+
+            if not is_first:
+                print("")
+            else:
+                is_first = False
+
+            self.device_ensure_scanned(device)
+            # print_device_info(device)
+            for ap in device.get_access_points():
+                # self.print_ap_info(ap)
+                ssid = self.ap_get_ssid(ap)
+                print(f'ssid = {ssid}')
+                if not text_exists_in_combobox(self.wifi_selector, ssid):
+                    self.wifi_selector.append_text(ssid)
+
     def on_output_device_changed(self, combo):
         device_name = combo.get_active_text()
         output_devices = self.pa.sink_list()
@@ -181,11 +235,68 @@ class UbuntuFrameControlWindow(Adw.ApplicationWindow):
         subprocess.run(['brightnessctl', 'set', str(brightness)])
         print(f"Brightness set to {brightness}%")
 
+    def device_needs_scan(self, device):
+        if device.get_client() is None:
+            # the device got deleted. We can forget about it.
+            return False
+        t = device.get_last_scan()
+        return t == 0 or t < NM.utils_get_timestamp_msec() - SCAN_THRESHOLD_MSEC
+ 
+    def device_ensure_scanned(self, device):
+        if os.getenv("NO_SCAN") == "1":
+            return
+
+        if not self.device_needs_scan(device):
+            return
+        
+        def handle_request_scan(device, params):
+            print('handle_request_scan')
+
+        # kick off a new scan.
+        device.request_scan_async(None)
+
+    def print_ap_info(self, ap):
+        strength = ap.get_strength()
+        frequency = ap.get_frequency()
+        flags = ap.get_flags()
+        wpa_flags = ap.get_wpa_flags()
+        rsn_flags = ap.get_rsn_flags()
+
+        t = ap.get_last_seen()
+        if t < 0:
+            last_seen = "never"
+        else:
+            t = time.clock_gettime(time.CLOCK_BOOTTIME) - t
+            last_seen = "%s sec ago" % (math.ceil(t),)
+
+        print("  - D-Bus path: %s" % (ap.get_path(),))
+        print("    SSID:       %s" % (self.ap_get_ssid(ap),))
+        print("    BSSID:      %s" % (ap.get_bssid(),))
+        print("    Last seen:  %s" % (last_seen,))
+        print("    Frequency:  %s" % (frequency,))
+        print("    Channel:    %s" % (NM.utils_wifi_freq_to_channel(frequency),))
+        print(
+            "    Strength:   %s%% : %s"
+            % (
+                strength,
+                NM.utils_wifi_strength_bars(strength),
+            )
+        )
+
+    def ap_get_ssid(self, ap):
+        if ap is None:
+            return "not connected"
+        ssid = ap.get_ssid()
+        if ssid is None:
+            return "no ssid"
+        return '%s' % (NM.utils_ssid_to_utf8(ssid.get_data()),)
+
 class UbuntuFrameControl(Adw.Application):
 
     def __init__(self):
         Adw.Application.__init__(self, application_id="com.github.kenvandine.UbuntuFrameControl", flags=Gio.ApplicationFlags.NON_UNIQUE)
         self.connect("activate", self.on_activate)
+
 
     def on_activate(self, app):
         win = UbuntuFrameControlWindow(app)
